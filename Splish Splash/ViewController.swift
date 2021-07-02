675ab97cf9f3e6912225cb6d54e040b93564a1a8
Proud19 <proud.mpala@hinge.co>
2021-07-02 08:30:37 -0700

    Initial Commit

---
+++ Splish Splash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Splish Splash
+//
+//  Created by Proud Mpala on 7/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
