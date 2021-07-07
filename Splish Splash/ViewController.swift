9064f3c16502834bd50fe004d72fa45c74777004
Proud19 <proud.mpala@hinge.co>
2021-07-07 15:07:21 -0700

    Added functionality to accomodate simelteneous touches to be treated as distinct touches

--- Splish Splash/ViewController.swift
+++ Splish Splash/ViewController.swift
@@ -8,25 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            self.touchedScreen(touch: touch)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tap = UITapGestureRecognizer(target: self, action: #selector(touchedScreen(touch:)))
-            view.addGestureRecognizer(tap)
+        //configure view
+        view.isMultipleTouchEnabled = true
     }
+
     
     
-    @objc func touchedScreen(touch: UITapGestureRecognizer) {
+    func touchedScreen(touch: UITouch) {
         let touchPoint = touch.location(in: self.view)
         let pulse = Splish(numberOfPulses: 1, radius: 130, position: touchPoint)
         pulse.animationDuration = 2
         pulse.backgroundColor = randomColor()
         touch.view?.layer.insertSublayer(pulse, above: touch.view?.layer)
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
-            self.makeSplash(touchPoint: touchPoint, splishRadius: 130, view: touch.view!)
+            self.makeSplash(touchPoint: touchPoint, splishRadius: 130, view: self.view)
             }
-        
+
     }
     
     func makeSplash(touchPoint: CGPoint, splishRadius: Float, view: UIView) {
@@ -34,44 +41,44 @@ class ViewController: UIViewController {
         let minDistance = Float(60.0)
         let numCircles = Int.random(in: 2...5)
         for _ in 0..<numCircles {
-            
+
             // generating random value circle center and radius
             var d_x = CGFloat(Float.random(in: minDistance...maxDistance))
             var d_y = CGFloat(Float.random(in: minDistance...maxDistance))
             let decider_x = Float.random(in: 0...1)
-            
+            let decider_y = Float.random(in: 0...1)
+
             if decider_x < 0.5 {
                 d_x = -d_x
             }
-            let decider_y = Float.random(in: 0...1)
             if decider_y < 0.5 {
                 d_y = -d_y
             }
-            
+
             let d_point = CGPoint(x: d_x + touchPoint.x, y: d_y + touchPoint.y)
             print(touchPoint)
             let radius_scale = Float.random(in: 0.2...0.4)
             let radius = CGFloat(splishRadius * radius_scale)
-            
+
             let pulse = Splish(numberOfPulses: 1, radius: radius, position: d_point)
             pulse.animationDuration = 2
             pulse.backgroundColor = randomColor()
             view.layer.insertSublayer(pulse, above: view.layer)
-           
+
         }
     }
-    
+
     func randomColor() -> CGColor {
         var floatRandom: CGFloat {
             CGFloat(Float.random(in: 0...1))
         }
-        
+
         let color = CGColor(red: floatRandom,
                             green: floatRandom,
                             blue: floatRandom,
                             alpha: 1)
         return color
-        
+
     }
 }
 
